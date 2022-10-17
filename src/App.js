a9703a2b072692afc1fc83b0e8e28840535e0bc2
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-10-17 22:48:38 +0530

    dark theam

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+// import { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 
 function App() {
+  const [mode, setMode]=useState('light')
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='gray'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+    }
+  }
   return (
     <>
-  <Navbar/> 
+  <Navbar mode={mode} toggleMode={toggleMode}/> 
   <div className="container">
-  <TextForm/>
+  <TextForm heading="Enter the text to analyze below" mode={mode}/>
   </div>
  
   </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,25 +1,55 @@
 import React,{useState} from 'react'
 
 
-export default function TextForm() {
+export default function TextForm(props) {
     const UpHandle =()=>{
-        console.log("UpperCase Button is clicked")
-        setText("Text time ")
+        // console.log("UpperCase Button is clicked");
+        let newText=text.toUpperCase();
+        setText(newText);
       
     }
-    const handle =()=>{
-        console.log("handle Button is clicked")
+    const handle =(event)=>{
+        // console.log("handle Button is clicked");
         setText(event.target.value)
     }
-    const [text, setText]= useState("Enter Your Text Here")
+    const [text, setText]= useState("Enter Your Text Here");
+    const [darkMode, setDarkMode]=useState({
+        color:'black',
+        backgroundColor:'white'
+    });
+    const [btnText, setBtnText]=useState("Enable Dark Mode")
+   const dark=()=>{
+        if(darkMode.color==='black'){
+            setDarkMode({
+                color:"white",
+                backgroundColor:'black'
+            })
+            setBtnText("Enable Light Mode")
+        }
+        else{
+            setDarkMode({
+                color:"black",
+                backgroundColor:'white'
+            })
+            setBtnText("Enable Dark Mode")
+        }
+    }
   return (
-    <div>
-         
+    <>
+    <div className='container' style={{color : props.mode==='dark'?'white':'black'}}>
+         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <label for="myBox" className="form-label" value={text} onChange={handle}>Enter Your Text Below</label>
-            <textarea className="form-control" id="myBox" rows="8"></textarea>
+            
+            <textarea   className="form-control" value={text} onChange={handle}  style={{backgroundColor:props.mode==='dark'?'gray':'white', color : props.mode==='dark'?'white':'black'}} id="myBox" rows="8" ></textarea>
         </div>
-        <button className="btn btp-primary" onClick={UpHandle}>Convert to UpperCase</button>
+        <button type="button" className="btn btp-primary" onClick={UpHandle}>Convert to UpperCase</button>
+        <button className="btn btp-primary" onClick={dark}>{btnText}</button>
     </div>
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}> 
+    <h1>Your Text </h1>
+    <p> {text.split(" ").length}word and {text.length}characters </p>
+    <p>{0.008*text.split(" ").length} Minute to read </p>
+    </div>
+    </>
   )
 }
